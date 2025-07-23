59ee39f6daa5e3c034800e50b064aa1e05b36214
Vinicius Silva <vini.silva1501.oli@gmail.com>
2025-07-23 11:36:59 -0300

    Add files via upload

--- Exercício01/script.js
+++ Exercício01/script.js
@@ -31,3 +31,15 @@ function div(){
     }
     res.textContent = "Não se divide por 0"
 }
+
+function med(){
+    var numero9 = parseFloat(document.getElementById("n9").value)
+    var numero10 = parseFloat(document.getElementById("n10").value)
+    var numero11 = parseFloat(document.getElementById("n11").value)
+    var numero12 = parseFloat(document.getElementById("n12").value)
+    var numero13 = parseFloat(document.getElementById("n13").value)
+    var val = document.getElementById("respMed")
+    val.textContent = ( numero9 + numero10 + numero11 + numero12 + numero13 ) / 5
+        
+    }
+    
